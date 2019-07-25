6dfe0ea1150cedb2d14a73ecb82032a4f62d36d3
kartikc96 <50267957+kartikc96@users.noreply.github.com>
2019-07-25 23:26:59 +0530

    Updates

---
+++ precache-manifest.7a08e1d6f4b27db428309a01a76cc3c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b1e6887c7865c66b2f81913876c3a2",
+    "url": "/react-testproject/index.html"
+  },
+  {
+    "revision": "83d71d3e95915a384b67",
+    "url": "/react-testproject/static/css/main.cea995b2.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-testproject/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "83d71d3e95915a384b67",
+    "url": "/react-testproject/static/js/main.48652575.chunk.js"
+  },
+  {
+    "revision": "cbac767da12403f7a763",
+    "url": "/react-testproject/static/js/runtime~main.7973c7e7.js"
+  }
+]);
